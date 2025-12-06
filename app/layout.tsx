ab61e08c6ed80dbbce2d4fe872e2cc47c72f646a
rmahipalsingh602-spec <rmahipalsingh602@gmail.com>
2025-12-06 12:46:26 +0530

    Updated DNA AI features

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PV48PPC5Q0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PV48PPC5Q0');
+            `,
+          }}
+        />
+      </head>
+
       <body className="min-h-screen overflow-x-hidden bg-black text-white">
         {children}
       </body>
